37a4c113dac270821644ae11dfb2f7dc4098220e
Mohamad23-05 <mohamad.kadabashi@outlook.de>
2023-09-14 16:59:52 +0200

    timeline infos

---
+++ darkmoka-portfolio/src/data/timeline.js
@@ -0,0 +1,32 @@
+export default[
+    {
+        year: '2019 - 2022',
+        title: 'A-Level Journey in Dresden',
+        discription: 'In 2019, I embarked on my A-level journey in Dresden, where my main focus was programming in C#. \
+        This educational phase concluded with my graduation in 2022, \
+        during which I had the opportunity to work on various projects and thoroughly enjoyed the experience.'
+
+    },
+    {
+        year: 'October 2022' ,
+        title: 'Commencement of University Studies',
+        discription: 'In October 2022, I began my university studies at HTW Dresden, majoring in Medieninformatik. \
+                        Throughout this academic journey, I delved into a wide range of subjects, including Java, C++, C, Bash, SQL, design, and mathematics.'
+
+    },
+    {
+        year: 'October 2022',
+        title: 'Entry into Fullstack Development',
+        discription: 'During the same month in 2022, I also embarked on my career as a fullstack developer in WordPress. \
+                        This role required me to learn PHP and other technologies such as DDev, Docker, Sass, and Node.js.'
+
+    },
+    {
+        year: '2023',
+        title: 'Expanding Web Development Skills',
+        discription: 'In 2023, I continued to broaden my web development skills. My growing comfort with HTML, CSS, and JavaScript prompted me to explore React and TypeScript. \
+                    Additionally, I discovered the benefits of using Tailwind CSS, and I am currently engaged in building projects with React.'
+
+    },
+    
+]
