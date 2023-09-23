80eea5ed0387b98fa593d8e518f315acbde00a1f
Mohamad23-05 <mohamad.kadabashi@outlook.de>
2023-09-23 18:46:32 +0200

    projects component

--- darkmoka-portfolio/src/components/Portfolio.jsx
+++ darkmoka-portfolio/src/components/Portfolio.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import portfolio from "../data/portfolio";
 import PortfolioItem from "./PortfolioItem";
 
-
 function Portfolio(){
     return(
+        
         <div className="flex md:flex-row items-center justify-center">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            
+            <div className="grid grid-cols-1 w-7/12 md:w-11/12 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            
                 {portfolio.map(project => (
                     <PortfolioItem
                         imgUrl={project.imgUrl}
--- darkmoka-portfolio/src/components/PortfolioItem.jsx
+++ darkmoka-portfolio/src/components/PortfolioItem.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 function PortfolioItem({title, imgUrl, stack, link}){
     return(
-        <div className="border-2 border-green-300 rounded-md overflow-hidden">
+        <div className="border-2 text-brwon-700 border-green rounded-md overflow-hidden dark:border-beige-500">
             <img 
             src={imgUrl} 
             alt="Portfolio"
-            className="w-full h-36 md:h-48 object-cover cursor-pointer"
+            className="w-full h-48 object-cover cursor-pointer"
             />
             <div className="w-full p-4">
-                <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">{title}</h3>
+                <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-bold">{title}</h3>
 
                 <p className="felx flex-wrap space-x-1 flex-row items-center justify-start text-xs md:text-sm">
                     {stack.map(item => (
-                        <span className="inline-block px-2 py-1 font-semibold border-2 border-green-300 rounded-md">
+                        <span className="inline-block p-1 font-semibold border-2 border-green rounded-md dark:border-beige-500">
                             {item}
                         </span>
                     ))}
