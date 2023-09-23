fa68d58cba336a1098d41e75b9463fa90745feab
Mohamad23-05 <mohamad.kadabashi@outlook.de>
2023-09-23 18:48:36 +0200

    importing infos from the js file

--- darkmoka-portfolio/src/components/intro.jsx
+++ darkmoka-portfolio/src/components/intro.jsx
@@ -1,13 +1,13 @@
 /*eslint no-unused-vars: ["error", { "args": "all" }]*/
-
 import React from "react";
+import myInfo from "../data/myInfo";
 
 function Intro(){
     return(
-        <div className="flex items-center justify-center flex-col text-center p-8 pd-6">
-            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 text-brown-400 font-bold">DarkMoka</h1>
+        <div className="flex items-center justify-start flex-col text-center p-8 pd-6">
+            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 dark:text-beige-500 font-bold">DarkMoka</h1>
             <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Web Developer</p>
-            <p className="text-sm max-w-xl mb-6 font-bold">This is my entrie Bio!</p>
+            <p className="text-sm max-w-xl mb-6 font-bold text-left">{myInfo.info}</p>
         </div>
    )
 }
---
+++ darkmoka-portfolio/src/data/myInfo.js
@@ -0,0 +1,6 @@
+export default {
+    info: "I'm Mohamad Kadabashi, and I have been living in Germany since I was sixteen.\
+    I started coding when I was thirteen, and I got very excited when I realized that programming is the future.\
+    So, I decided to become a software engineer. Now, in Germany, I have the opportunity to study at the university in Dresden/Germany, and I am currently in my second year of my major."
+  }
+  
